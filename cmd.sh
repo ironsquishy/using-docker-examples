e8abdedbe58d1bc9436d73a775993b40894a37dd
ironsquishy <spacecore88@gmail.com>
2017-02-18 12:10:40 -0600

    yolo

--- cmd.sh
+++ cmd.sh
@@ -8,12 +8,3 @@ else
   exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py \
              --callable app --stats 0.0.0.0:9191
 fi
-
-if ["$ENV" = 'DEV']; then
-  echo "Running Development Server"
-  exec python "identidock.py"
-else
-  echo "Running Production Server"
-  exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py \
-             --callable app --stats 0.0.0.0:9191
-fi
